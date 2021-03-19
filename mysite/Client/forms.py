9cf2d2e249c40efa4961209198fc9f0cd25e1194
Galliantes <80777009+Galliantes@users.noreply.github.com>
2021-03-19 10:07:11 +0100

    Delete mysite/Client directory

--- mysite/Client/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-from .models import Commande
-
-class ArticleForm(ModelForm):
-    class Meta:
-        model = Commande
-        fields = ['Numero_Unique', 'Commande_individu']
-

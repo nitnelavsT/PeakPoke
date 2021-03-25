2b1c1a28b491b4cc67e5cc3a1f6818065b5df3f1
Mete ERDOGAN <80773274+MeteErdogan@users.noreply.github.com>
2021-03-25 20:41:17 +0100

    Add files via upload

--- settings.py
+++ settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'pc&25y)(39$q1o*ujqj%vlvu1%78p2!+#%-tmi44pyd8#)mgfb'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1','peakpoke-gpi.herokuapp.com/']
+ALLOWED_HOSTS = ['127.0.0.1','peakpoke-gpi.herokuapp.com']
 
 
 # Application definition

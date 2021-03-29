0a36dd381435d77b1d078ec30541fc9bf71b09c9
Thomas BONNET <80777009+Galliantes@users.noreply.github.com>
2021-03-29 12:18:58 +0200

    Mise à jour des champs pour Article

--- Article/models.py
+++ Article/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 from Client.models import Commande
+import uuid
 
 class Article(models.Model):
     #commande_article = models.ForeignKey(Commande, on_delete=models.CASCADE, default= "0")
-    numero_article = models.IntegerField()
+    #numero_unique = models.CharField(max_length=3, blank=True,editable=False,unique=True, default=create_ref_number())
+    numero_unique= models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     nom_article = models.CharField(max_length=100)
     quantite_article = models.IntegerField(default=0)
+    prix = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+
+    def __str__(self):
+        return str(self.numero_unique) + str(self.nom_article) + "€" + str(self.prix)

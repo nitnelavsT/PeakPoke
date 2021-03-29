acf9836b3eedd915acba462581e9bbb6991fbcbb
Thomas BONNET <80777009+Galliantes@users.noreply.github.com>
2021-03-29 12:21:34 +0200

    Mise à jour interface base Article
    Les champs affichées sur l'interface Articles sont maintenant nom_article et numero_unique

--- Article/admin.py
+++ Article/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import Article
 
-admin.site.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('nom_article', 'numero_unique')
+
+admin.site.register(Article, ArticleAdmin)

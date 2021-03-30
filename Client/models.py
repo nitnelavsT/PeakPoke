18f8649491860a3cf9f0b9e383fa2b1e41e37bbf
Thomas BONNET <80777009+Galliantes@users.noreply.github.com>
2021-03-30 14:09:35 +0200

    Ajout Email

--- Client/models.py
+++ Client/models.py
@@ -18,7 +18,7 @@ class Client(models.Model):
     phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$',
                                  message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
     Numero_Telephone = models.CharField(validators=[phone_regex], max_length=17, blank=True)  # validators should be a list
-
+    Email = models.EmailField(max_length=254, blank=True)
 
 
 

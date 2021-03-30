35adeacb002b07978b976759ab66b17a07536c84
Mete ERDOGAN <80773274+MeteErdogan@users.noreply.github.com>
2021-03-30 16:45:50 +0200

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -5,6 +5,8 @@ from django.contrib import admin
 from django.urls import include, path
 from . import views
 
+from django.views.static import serve
+from django.conf.urls import url
 
 
 
@@ -15,6 +17,9 @@ urlpatterns = [ #récupération des urls des autres applications et de l'outils 
     path('Article/', include('Article.urls')),
     path('admin/', admin.site.urls),
     path('__debug__/', include(debug_toolbar.urls)),
+    
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 
 if settings.DEBUG:

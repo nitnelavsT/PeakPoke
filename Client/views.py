d842f055667b6474a4aa652acca4bac91bca617c
Thomas BONNET <80777009+Galliantes@users.noreply.github.com>
2021-03-30 11:39:46 +0200

    Mise à jour des commentaires

--- Client/views.py
+++ Client/views.py
@@ -7,55 +7,33 @@ from django.views import generic
 from django.template import loader
 
 
-class IndexView(generic.ListView):
-    template_name = 'Client/index.html'
-    context_object_name = 'lastest_Client'
+class IndexView(generic.ListView): # Vue de la page d'accueil du site 
+    template_name = 'Client/index.html' #le template pour la page d'accueil
+    context_object_name = 'lastest_Client' #nom du contexte pour le fichier html (template)
 
-    def get_queryset(self):
+    def get_queryset(self): #méthod de récupération du modèle Client
         """:return le dernier Client"""
         return Client.objects.order_by('Nom')  # Permet de trier les client par nom
 
 
 
-def detail(request, Client_id):
+def detail(request, Client_id): #affichage des détails Client
     client = Client.objects.get(pk=Client_id)
     message = "Le nom du Client est {}".format(Client.Nom, Client)
     context = dict()
     return render(request, 'Client/index.html', context)
-#
-#
-# class DetailView(generic.DetailView):
-#     model = Client
-#     template_name = 'Client/detail.html'
-#
-#
-# class MontantView(generic.DetailView):
-#     model = Client
-#     template_name = 'Client/Montant.html'
 
 
-def commande_article(request, Client_id):
+
+def commande_article(request, Client_id): #affichage des commandes Client
     client = get_object_or_404(Client, pk=Client_id)
     try:
         selected_commande = client.commande_set.get(pk=request.POST['Commande'])
     except (KeyError, Commande.DoesNotExist):
         # Reaffiche les articles de la commande
         return render(request, 'Client/detail.html',
-                      {'Client': Client, 'error_message': "Vous n'avez pas selectionner de commande."})
+                      {'Client': Client, 'error_message': "Vous n'avez pas selectionner de commande."}) #render : pour lier une méthode à un template
     else:
         selected_commande.Numero_Uniques += 1
         selected_commande.save()
         return HttpResponseRedirect(reverse('Client:Montant', args=(Client_id,)))
-
-
-
-
-
-
-'''def index(request):
-    template = loader.get_template('Client/index.html')
-    return HttpResponse(template.render(request=request))'''
-
-
-
-

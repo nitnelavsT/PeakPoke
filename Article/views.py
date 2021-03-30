7062fbf41ca9548575a37a9a8bc98dff5d84fc97
Thomas BONNET <80777009+Galliantes@users.noreply.github.com>
2021-03-30 11:47:35 +0200

    commentaires

--- Article/views.py
+++ Article/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(requet):
+def index(request): # pour ne pas laisser de blanc sur la page article
     return HttpResponse("Salut je suis Article")

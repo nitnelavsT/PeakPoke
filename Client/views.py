f174225d6b0603167ba5e577decbdadab3c46291
Thomas BONNET <80777009+Galliantes@users.noreply.github.com>
2021-04-08 17:50:57 +0200

    maj

--- Client/views.py
+++ Client/views.py
@@ -1,27 +1,20 @@
-from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from .models import Client, Commande
-from django.template import loader
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
-from django.views import generic
-from django.template import loader
 
 
-class IndexView(generic.ListView): # Vue de la page d'accueil du site 
-    template_name = 'Client/index.html' #le template pour la page d'accueil
-    context_object_name = 'lastest_Client' #nom du contexte pour le fichier html (template)
-
-    def get_queryset(self): #méthod de récupération du modèle Client
-        """:return le dernier Client"""
-        return Client.objects.order_by('Nom')  # Permet de trier les client par nom
+def IndexView(request): # Vue de la page d'accueil du site
+    client = Client.objects.all()
+    context = {'client': client}
+    return render(request, 'Client/ClientList.html', context)
 
 
 
 def detail(request, Client_id): #affichage des détails Client
     client = Client.objects.get(pk=Client_id)
-    message = "Le nom du Client est {}".format(Client.Nom, Client)
-    context = dict()
-    return render(request, 'Client/index.html', context)
+    context = {'client': client}
+    return render(request, 'Client/DetailC.html', context)
 
 
 
@@ -32,8 +25,10 @@ def commande_article(request, Client_id): #affichage des commandes Client
     except (KeyError, Commande.DoesNotExist):
         # Reaffiche les articles de la commande
         return render(request, 'Client/detail.html',
-                      {'Client': Client, 'error_message': "Vous n'avez pas selectionner de commande."}) #render : pour lier une méthode à un template
+                      {'Client': Client, 'error_message': "Vous n'avez pas selectionner de commande."})
     else:
         selected_commande.Numero_Uniques += 1
         selected_commande.save()
         return HttpResponseRedirect(reverse('Client:Montant', args=(Client_id,)))
+
+

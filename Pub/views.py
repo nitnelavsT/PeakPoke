026d81fbb36b1b149a33aa4862d2c5a39ad17701
Mete ERDOGAN <80773274+MeteErdogan@users.noreply.github.com>
2021-04-10 13:20:17 +0200

    Update views.py

--- Pub/views.py
+++ Pub/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def index(request): # pour ne pas laisser de blanc sur la page article
+def index(request): # pour ne pas laisser de blanc sur la page pub
     return HttpResponse("Salut je suis la Pub")

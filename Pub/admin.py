19d382afb121c9cf218372d3b8133f598df2261f
Mete ERDOGAN <80773274+MeteErdogan@users.noreply.github.com>
2021-04-12 18:15:43 +0200

    Update admin.py

--- Pub/admin.py
+++ Pub/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from .models import Pub
+from tinymce.widgets import TinyMCE
+from django.db import models
+from django import forms
 
 class PubAdmin(admin.ModelAdmin):
     list_display = ('titre','auteur')
+    body = forms.CharField(widget=TinyMCE())
 
 admin.site.register(Pub,PubAdmin)
